832d79988f49195736e468d948e1676307b50bc5
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2023-07-23 10:14:53 +0100

    Add Clippy to bookmarkers script

--- src/bookmarker.ts
+++ src/bookmarker.ts
@@ -1,14 +1,44 @@
-// This script is a work in progress
-
+import { Table } from "https://deno.land/x/cliffy@v1.0.0-rc.2/table/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
+import { Input } from "https://deno.land/x/cliffy@v1.0.0-rc.2/prompt/mod.ts";
+import { tty } from "https://deno.land/x/cliffy@v1.0.0-rc.2/ansi/tty.ts";
+import { colors } from "https://deno.land/x/cliffy@v1.0.0-rc.2/ansi/colors.ts";
 import bookmarks from "../data/bookmarks.json" assert { type: "json" };
+
+const title = colors.bold.bgBrightGreen.black;
 
 type Collection = { title: string; contents: Bookmark[] };
 type Bookmark = { href: string; label: string };
 
-bookmarks.forEach((item: Collection) => {
-  console.log(item.title);
+await new Command()
+  .name("Bookmarker")
+  .version("0.1.0")
+  .description("Recall your bookmarks from the command line.")
+  .parse(Deno.args);
 
-  item.contents.forEach((bookmark: Bookmark) => {
-    console.log(`${bookmark.label}\n`);
-  });
+tty.cursorSave
+  .cursorHide
+  .cursorTo(0, 0)
+  .eraseScreen();
+
+const filterString: string = await Input.prompt({
+  message: `Filter: `,
+  suggestions: bookmarks.map((item: Collection) => item.title)
+});
+
+
+bookmarks.forEach((item: Collection) => {
+  if (item.title.toLowerCase().includes(filterString.toLowerCase())) {
+    console.log(title(item.title));
+
+    new Table()
+      .header(["Label", "URL"])
+      .body(item.contents.map((bookmark: Bookmark) => [bookmark.label, bookmark.href]))
+      .minColWidth([20, 30])
+      .maxColWidth([20, 30])
+      .padding(1)
+      .indent(2)
+      .border()
+      .render();
+  }
 });
